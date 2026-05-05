a4c7f4265865692b1ff4f97bd4f71c1394eec2d5
houw9449-hue <houw9449@gmail.com>
2026-05-05 15:53:46 +0800

    Create bib.js

---
+++ data/generated/bib.js
@@ -0,0 +1,162 @@
+const generatedBibEntries = {
+    "araujo2017classification": {
+        "author": "Araújo, Teresa and Aresta, Guilherme and Castro, Eduardo and Rouco, José and Aguiar, Paulo and Eloy, Catarina and Polónia, António and Campilho, Aurélio",
+        "doi": "10.1371/journal.pone.0177544",
+        "journal": "PLOS ONE",
+        "keywords": "deep_learning, classification, breast_cancer, CNN",
+        "number": "6",
+        "pages": "e0177544",
+        "title": "Classification of breast cancer histology images using convolutional neural networks",
+        "type": "article",
+        "volume": "12",
+        "year": "2017"
+    },
+    "chen2022scaling": {
+        "author": "Chen, Richard J. and Chen, Chengkuan and Li, Yang and Chen, Tiffany Y. and Trister, Andrew D. and Krishnan, Rahul G. and Mahmood, Faisal",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR52688.2022.01567",
+        "keywords": "transformer, self_supervised_learning, histopathology, whole_slide_image",
+        "pages": "9184--9195",
+        "title": "Scaling vision transformers to gigapixel images via hierarchical self-supervised learning",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "fridadar2018gan": {
+        "author": "Frid-Adar, Maayan and Diamant, Idit and Klang, Eyal and Amitai, Michal and Goldberger, Jacob and Greenspan, Hayit",
+        "doi": "10.1016/j.neucom.2018.09.013",
+        "journal": "Neurocomputing",
+        "keywords": "data_augmentation, GAN, class_imbalance, medical_imaging",
+        "pages": "321--331",
+        "title": "GAN-based synthetic medical image augmentation for increased CNN performance in liver lesion classification",
+        "type": "article",
+        "volume": "321",
+        "year": "2018"
+    },
+    "huang2017densely": {
+        "author": "Huang, Gao and Liu, Zhuang and Van Der Maaten, Laurens and Weinberger, Kilian Q.",
+        "booktitle": "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR.2017.243",
+        "keywords": "deep_learning, DenseNet, CNN, classification",
+        "pages": "4700--4708",
+        "title": "Densely connected convolutional networks",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "ilse2018attention": {
+        "author": "Ilse, Maximilian and Tomczak, Jakub M. and Welling, Max",
+        "booktitle": "Proceedings of the 35th International Conference on Machine Learning (ICML)",
+        "doi": "10.48550/arXiv.1802.04712",
+        "keywords": "attention, multiple_instance_learning, interpretability, histopathology",
+        "title": "Attention-based deep multiple instance learning",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "johnson2019survey": {
+        "author": "Johnson, Justin M. and Khoshgoftaar, Taghi M.",
+        "doi": "10.1186/s40537-019-0192-5",
+        "journal": "Journal of Big Data",
+        "keywords": "class_imbalance, deep_learning, oversampling, survey",
+        "number": "1",
+        "pages": "27",
+        "title": "Survey on deep learning with class imbalance",
+        "type": "article",
+        "volume": "6",
+        "year": "2019"
+    },
+    "li2025enhancing": {
+        "author": "Li, Xingwang and Lin, Qiang and Khan, Fawad and Kumari, Saru and Alenazi, Mohammed J. F. and Yang, Jian",
+        "doi": "10.1109/TCE.2025.3545963",
+        "journal": "IEEE Transactions on Consumer Electronics",
+        "keywords": "federated_learning, split_learning, breast_cancer, deep_learning, BreakHis",
+        "number": "2",
+        "pages": "6673--6685",
+        "title": "Enhancing cancer detection capabilities in medical consumer electronics through split federated learning and deep learning optimization",
+        "type": "article",
+        "volume": "71",
+        "year": "2025"
+    },
+    "mcmahan2017communication": {
+        "author": "McMahan, Brendan and Moore, Eider and Ramage, Daniel and Hampson, Seth and y Arcas, Blaise Agüera",
+        "booktitle": "Proceedings of the 20th International Conference on Artificial Intelligence and Statistics (AISTATS)",
+        "doi": "10.48550/arXiv.1602.05629",
+        "keywords": "federated_learning, FedAvg, distributed_learning, privacy",
+        "title": "Communication-efficient learning of deep networks from decentralized data",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "rieke2020future": {
+        "author": "Rieke, Nicola and Hancox, Jonny and Li, Wenqi and Milletari, Fausto and Roth, Holger R. and Albarqouni, Shadi and Bakas, Spyridon and Galtier, Mathieu N. and Landman, Bennett A. and Maier-Hein, Klaus and Ourselin, Sebastien and Sheller, Micah and Summers, Ronald M. and Trask, Andrew and Xu, Daguang and Baust, Maximilian and Cardoso, M. Jorge",
+        "doi": "10.1038/s41746-020-00323-1",
+        "journal": "NPJ Digital Medicine",
+        "keywords": "federated_learning, digital_health, privacy, medical_imaging",
+        "number": "1",
+        "pages": "119",
+        "title": "The future of digital health with federated learning",
+        "type": "article",
+        "volume": "3",
+        "year": "2020"
+    },
+    "selvaraju2017gradcam": {
+        "author": "Selvaraju, Ramprasaath R. and Cogswell, Michael and Das, Abhishek and Vedantam, Ramakrishna and Parikh, Devi and Batra, Dhruv",
+        "booktitle": "Proceedings of the IEEE International Conference on Computer Vision (ICCV)",
+        "doi": "10.1109/ICCV.2017.74",
+        "keywords": "interpretability, Grad_CAM, visualization, deep_learning",
+        "pages": "618--626",
+        "title": "Grad-CAM: Visual explanations from deep networks via gradient-based localization",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "spanhol2016dataset": {
+        "author": "Spanhol, Fabio A. and Oliveira, Luiz S. and Petitjean, Caroline and Heutte, Laurent",
+        "doi": "10.1109/TBME.2015.2496264",
+        "journal": "IEEE Transactions on Biomedical Engineering",
+        "keywords": "breast_cancer, dataset, BreakHis, histopathology, classification",
+        "number": "7",
+        "pages": "1455--1462",
+        "title": "A dataset for breast cancer histopathological image classification",
+        "type": "article",
+        "volume": "63",
+        "year": "2016"
+    },
+    "srinidhi2021deep": {
+        "author": "Srinidhi, Ciga Lavanya and Ciga, Ozan and Martel, Anne L.",
+        "doi": "10.1016/j.media.2020.101813",
+        "journal": "Medical Image Analysis",
+        "keywords": "survey, histopathology, deep_learning, computational_pathology",
+        "pages": "101813",
+        "title": "Deep neural network models for computational histopathology: A survey",
+        "type": "article",
+        "volume": "67",
+        "year": "2021"
+    },
+    "tajbakhsh2016convolutional": {
+        "author": "Tajbakhsh, Nima and Shin, Jae Y. and Gurudu, Suryakanth R. and Hurst, R. Todd and Kendall, Christopher B. and Gotway, Michael B. and Liang, Jianming",
+        "doi": "10.1109/TMI.2016.2535302",
+        "journal": "IEEE Transactions on Medical Imaging",
+        "keywords": "transfer_learning, fine_tuning, CNN, medical_imaging",
+        "number": "5",
+        "pages": "1299--1312",
+        "title": "Convolutional neural networks for medical image analysis: Full training or fine tuning?",
+        "type": "article",
+        "volume": "35",
+        "year": "2016"
+    },
+    "tan2019efficientnet": {
+        "author": "Tan, Mingxing and Le, Quoc V.",
+        "booktitle": "Proceedings of the 36th International Conference on Machine Learning (ICML)",
+        "doi": "10.48550/arXiv.1905.11946",
+        "keywords": "EfficientNet, model_scaling, CNN, classification",
+        "title": "EfficientNet: Rethinking model scaling for convolutional neural networks",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "vepakomma2018split": {
+        "author": "Vepakomma, Praneeth and Gupta, Otkrist and Swedish, Tristan and Raskar, Ramesh",
+        "doi": "10.48550/arXiv.1812.00564",
+        "journal": "arXiv preprint arXiv:1812.00564",
+        "keywords": "split_learning, federated_learning, privacy, medical_imaging",
+        "title": "Split learning for health: Distributed deep learning without sharing raw patient data",
+        "type": "article",
+        "year": "2018"
+    }
+};
